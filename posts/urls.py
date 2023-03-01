244b0920b998cff62e030c1c06f45cbe02d8b22a
Nurbek <unurbek499@gmail.com>
2023-03-01 17:47:51 +0600

    homework_3

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,10 @@
-from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
+from posts.views import  get_index, get_about, get_contacts#,hello,
 
+urlpatterns = [
+    # path("hello/", hello, name="hello-view"),
+    path("", get_index, name="index-page"),
+    path("about/", get_about, name="about-page"),
+    path("contacts/", get_contacts, name="contacts-page"),
 ]
